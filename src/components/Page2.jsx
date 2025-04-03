f589c11dbb6452e709ee83cb62aba30da52341d1
vikraman1653763 <vikraman1653763@gmail.com>
2025-04-03 10:35:51 +0530

    03-04-2025

--- src/components/Page2.jsx
+++ src/components/Page2.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProductIntro from './Page2/ProductIntro'
 import Application from './Page2/Application'
 import Product from './Page2/Product'
-import Solutions from './Page2/Solutions.JSX'
+import Solutions from './Page2/Solutions.jsx'
 
 export default function Page2() {
   return (
